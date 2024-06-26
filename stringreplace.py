19dbbc7d2b661d06c71d95ca130b07d164a08694
KumarNikhilSingh <nikhilniku24@gmail.com>
2024-06-26 13:47:28 +0530

    replace parts of string

---
+++ stringreplace.py
@@ -0,0 +1,6 @@
+def string_replace(string):
+    new_string = string.replace("Kumar", "Singh")
+    return new_string
+string = "My name is Nikhil Kumar"
+new_string = string_replace(string)
+print("Replaced string is: ", new_string)
